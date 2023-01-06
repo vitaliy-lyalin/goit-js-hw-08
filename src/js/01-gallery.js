695896e12eab854b4d40c0199971d9e71adaf01a
Vitaliy Lyalin <vitaliy.lyalin@gmail.com>
2023-01-07 00:09:52 +0200

    Add galleryItems markup, styles and links of simple-lightbox library

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,35 @@
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-const hello = 'hello world';
-console.log(hello);
-
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+createGalleryItemsMarkup();
+
+function createGalleryItemsMarkup() {
+  const markup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`;
+    })
+    .join('');
+
+  galleryContainer.insertAdjacentHTML('beforeend', markup);
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});

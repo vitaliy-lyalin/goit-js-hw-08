d08d2d756304dd00ec3781e105560a0157f02b5c
Vitaliy Lyalin <vitaliy.lyalin@gmail.com>
2023-01-07 14:12:48 +0200

    Add and initialize libraries vimeo player, lodash.throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const vimeoPlayer = document.querySelector('iframe');
+
+const player = new Player(vimeoPlayer);
+
+player.on('play', function () {
+  console.log('played the video!');
+});

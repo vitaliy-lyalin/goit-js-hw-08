b91de312109cd5571a639eec9c821bd6134bc973
Vitaliy Lyalin <vitaliy.lyalin@gmail.com>
2023-01-07 18:14:35 +0200

    Add script JS for 02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,6 +5,17 @@ const vimeoPlayer = document.querySelector('iframe');
 
 const player = new Player(vimeoPlayer);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+localStorage.getItem('videoplayer-current-time')
+  ? player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  : 0;
+
+player.on(
+  'timeupdate',
+  throttle(function ({ seconds }) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+    console.log(
+      'current time seconds:',
+      localStorage.getItem('videoplayer-current-time')
+    );
+  }, 1000)
+);
